c431615e8e4a7fea9a5f48dc544efa7bea6fb07b
Saptashrungi <saptashrungib@gmail.com>
2020-04-15 15:17:44 +0530

    latest socket.io implementation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-var http = require('http').createServer(app)
-var io = require('socket.io')(http);
+var server = require('http').Server(app)
+var io = require('socket.io').listen(server);
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +15,6 @@ io.on('connection', function(socket){
   });
 });
 
-app.listen(process.env.PORT || 3000,  function() {
+server.listen(process.env.PORT || 3000,  function() {
   console.log('Started listening on port 3000');
 })
