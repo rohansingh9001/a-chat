03de30891f660e535298a8324303a58dd1396ee8
Saptashrungi <saptashrungib@gmail.com>
2020-04-15 18:48:10 +0530

    socket.io implementation

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app)
-var io = require('socket.io').listen(server);
-
+var server = require('http').Server(app);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
+var io = require('./server/socket/index.js');
+io.attach(server)
 io.on('connection', function(socket){
   console.log('Client connected');
   socket.on('disconnect', function(){
     console.log('Client disconnected');
   });
 });
-
 server.listen(process.env.PORT || 3000,  function() {
   console.log('Started listening on port 3000');
 })
