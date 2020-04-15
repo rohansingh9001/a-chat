166d5cc8242972af56f92f4dde0e7f50a6413eb4
Saptashrungi <saptashrungib@gmail.com>
2020-04-15 14:27:13 +0530

    latest

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app)
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.sendFile(__dirname + '/index.html');
 })
+
+io.on('connection', function(socket){
+  console.log('Client connected');
+  socket.on('disconnect', function(){
+    console.log('Client disconnected');
+  });
+});
 
 app.listen(process.env.PORT || 3000,  function() {
   console.log('Started listening on port 3000');
