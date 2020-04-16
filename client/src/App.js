1d71448252dbd3284a2521cb210e996fafe15fa9
Rohan Singh <rohansingh9001>
2020-04-16 15:44:45 +0530

    Added the UI for chat using React

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+import Header from "./components/Header";
+
+// A Random colour generator to generate random avatars.
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+
+class App extends React.Component {
+  // State contains a messages array.
+  state = {
+    messages: [
+      // Hard coded the first message as of now.
+      {
+        text: "This is a test message!",
+        member: {
+          color: randomColor(),
+        }
+      }
+    ],
+    member: {
+      color: randomColor()
+    }
+  }
+
+  // Function to handle a sent message event.
+  onSendMessage = (message) => {
+    const messages = this.state.messages
+    messages.push({
+      text: message,
+      member: this.state.member
+    })
+    this.setState({messages: messages});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input
+          onSendMessage={this.onSendMessage}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
